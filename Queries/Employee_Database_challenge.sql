c12bdfe59d10da5786e8d77815253076e31c39ce
Conor Hanson <condorhanson@gmail.com>
2022-05-12 11:59:46 -0500

    updated table queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -15,8 +15,7 @@ where
 	e.birth_date >= '1952-01-01'
 	and e.birth_date <= '1955-12-31'
 	and ti.to_date = '9999-01-01'
-order by emp_no
-limit 10;
+order by emp_no;
 
 -- group by employee title and count total of employees with each title who are
 -- about to retire.
@@ -32,8 +31,7 @@ where
 	and e.birth_date <= '1955-12-31'
 	and t.to_date = '9999-01-01'
 group by t.title
-order by title_count desc
-limit 10;
+order by title_count desc;
 
 -- Queries to find employees eligible for mentorship program
 select distinct on (e.emp_no)
@@ -44,7 +42,7 @@ e.birth_date,
 de.from_date,
 de.to_date,
 ti.title
---into mentorship_eligibility
+into mentorship_eligibility
 from
 	employees as e
 	join dept_emp as de on e.emp_no = de.emp_no
@@ -52,5 +50,4 @@ from
 where
 	e.birth_date between '1965-01-01' and '1965-12-31'
 	and ti.to_date = '9999-01-01'
-order by emp_no
-limit 10;
+order by emp_no;
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 The purpose of this project was to help Pewlett Hackard prepare for a large exodus of retiring employees. First, we determined the number of retiring employees based on their title. Second, we as identified employees who are eligible to take part in a mentorship program prior to their future retirement. This would ensure the incoming employees would be properly trained, thereby limiting the disruption to Pewlett Hackard's operations.
 
 ## Results
-
+- The Production and Development departments have the largest number of retiring employees (8174 and 9281 respectively). These departments will require the greatest support into which to mentor employees.
+- 
 
 ## Summary
