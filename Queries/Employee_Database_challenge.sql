36b1009d2440260e934a279c35c52bdcfe2d03d6
Conor Hanson <condorhanson@gmail.com>
2022-05-12 09:55:23 -0500

    on deliv 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,44 +1,36 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
-
+-- Use Dictinct with Orderby to remove duplicate rows, exclude employees that
+-- have left the company, and fall in retirement range (1952-1955 birthday)
 select 
 distinct on(e.emp_no) e.emp_no,
 	e.first_name,
 	e.last_name,
-	e.birth_date,
-	t.title,
-	
+	ti.title,
+	ti.from_date,
+	ti.to_date
+into retirement_titles
 from
-	employees e
-	join titles t on e.emp_no = t.emp_no
+	employees as e
+	join titles as ti on e.emp_no = ti.emp_no
 where
 	e.birth_date >= '1952-01-01'
 	and e.birth_date <= '1955-12-31'
-	and t.to_date = "9999-01-01"
+	and ti.to_date = '9999-01-01'
 order by emp_no
 limit 10;
 
--- group by employee title and count total of with each title
+-- group by employee title and count total of employees with each title who are
+-- about to retire.
 select 
-t.title,
-count(e.emp_no) as title_count
-
+count(e.emp_no) as title_count,
+t.title
+into retiring_titles_count
 from
-	employees e
+	employees as e
 	join titles t on e.emp_no = t.emp_no
 where
 	e.birth_date >= '1952-01-01'
 	and e.birth_date <= '1955-12-31'
-	and t.to_date = "9999-01-01"
-
+	and t.to_date = '9999-01-01'
 group by t.title
 order by title_count desc
 limit 10;
