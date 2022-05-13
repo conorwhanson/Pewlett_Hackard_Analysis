ac181f69fdbfe1cea887661edfed9122cc02f4a1
Conor Hanson <condorhanson@gmail.com>
2022-05-12 21:43:42 -0500

    deliv 1 & 2 done

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,7 +1,5 @@
--- Use Dictinct with Orderby to remove duplicate rows, exclude employees that
--- have left the company, and fall in retirement range (1952-1955 birthday)
-select 
-distinct on(e.emp_no) 
+-- DELIVERABLE 1: Join Employee table and Title table conditioned on birthdate
+select distinct on (e.emp_no)
 	e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -15,40 +13,54 @@ from
 where
 	e.birth_date >= '1952-01-01'
 	and e.birth_date <= '1955-12-31'
-	and ti.to_date = '9999-01-01'
 order by emp_no;
 
--- group by employee title and count total of employees with each title who are
--- about to retire.
-select 
-count(e.emp_no) as title_count,
-t.title
-into retiring_titles_count
-from
-	employees as e
-	join titles t on e.emp_no = t.emp_no
-where
-	e.birth_date >= '1952-01-01'
-	and e.birth_date <= '1955-12-31'
-	and t.to_date = '9999-01-01'
-group by t.title
-order by title_count desc;
+-- group by most recent employee title
+select distinct on (emp_no)
+emp_no,
+first_name,
+last_name,
+title
+into unique_titles
+from retirement_titles
+where to_date = '9999-01-01'
+order by emp_no, to_date desc;
+
+-- find # of employees by most recent title who are ready to retire
+select count(title) as title_count,
+title
+into retiring_titles
+from unique_titles
+group by title
+order by title_count desc
 
 -- Queries to find employees eligible for mentorship program
 select distinct on (e.emp_no)
-e.emp_no,
-e.first_name,
-e.last_name,
-e.birth_date,
-de.from_date,
-de.to_date,
-ti.title
-into mentorship_eligibility
-from
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+from 
 	employees as e
 	join dept_emp as de on e.emp_no = de.emp_no
 	join titles as ti on e.emp_no = ti.emp_no
-where
+where 
 	e.birth_date between '1965-01-01' and '1965-12-31'
 	and ti.to_date = '9999-01-01'
-order by emp_no;
+order by 
+	e.emp_no, ti.to_date desc
+
+-- PROF BOOTH DELV 2
+select *
+from employees as e
+join dept_emp as de on e.emp_no = de.emp_no
+join titles as ti on e.emp_no = ti.emp_no
+where
+	e.birth_date >= '1952-01-01'
+	and e.birth_date <= '1955-12-31'
+	and ti.to_date  = '9999-01-01'
+	order by e.emp_no
+limit 10
